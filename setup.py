a12bdb4bfd5feeb8ac0c4ec7903b1f378bb945c5
Roshan Mundekar <155573532+roshan501@users.noreply.github.com>
2025-06-02 22:59:30 +0530

    add new pro

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project",
+    version= '0.0.0',
+    author="roshan Mundekar",
+    author_email="roshan.mundekar@algorisys.com",
+    packages= find_packages(),
+    install_requires = []
+)
